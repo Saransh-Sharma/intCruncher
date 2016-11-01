db1a01e17223c2f14b62037943532a78f8ea9244
Saransh Sharma <saransh1337@gmail.com>
2016-11-01 21:43:07 +0530

    add email server, ci, sam vm to launch

---
+++ intAllScripts.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+nohup /Users/saransh/codeMonkey/startEmailServer.sh &
+nohup /Users/saransh/codeMonkey/droidBuilderLauncher.sh &
+nohup /Users/saransh/codeMonkey/Projects/vostok/AVD_Sam_Harris.sh &
